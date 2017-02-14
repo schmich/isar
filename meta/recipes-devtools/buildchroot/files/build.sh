a480ee12c8cbd6f9d065d9611b0eb6fe7553c56c
Frank Lenormand <flenormand@ilbers.de>
2017-02-14 15:34:04 +0100

    Fix dependency installation in the build chroot

--- meta/recipes-devtools/buildchroot/files/build.sh
+++ meta/recipes-devtools/buildchroot/files/build.sh
@@ -12,7 +12,7 @@ cd $1
 DEPS=`perl -ne 'next if /^#/; $p=(s/^Build-Depends:\s*/ / or (/^ / and $p)); s/,|\n|\([^)]+\)//mg; print if $p' < debian/control`
 
 # Install deps
-apt-get install $DEPS
+apt-get install -y $DEPS
 
 # Build the package
 dpkg-buildpackage
